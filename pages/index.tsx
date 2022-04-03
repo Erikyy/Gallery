319c7744bf5f8d2650d4fd37f8204db256aa4eca
Erikyy <erikyl@hotmail.com>
2022-04-03 19:56:42 +0300

    Update stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
+import { fetchPosts } from '../lib/client'
 
 const Home: NextPage = () => {
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
   return (
     <div>
       
